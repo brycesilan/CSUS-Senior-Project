93cc5c05baf55e1098705b7e988a9e1e494ef0be
Tejpal Lalihiana <tejpalssb23@gmail.com>
2022-04-21 20:19:28 -0700

    Correct pH sensor Arduino code

--- Software/Arduino Code/pH.ino
+++ Software/Arduino Code/pH.ino
@@ -45,20 +45,3 @@ void loop() {
   Serial.println(ph_act);
   delay(1000);
 }
-Calibrate pH:
-int pH_Value; 
-float Voltage;
- 
-void setup() 
-{ 
-  Serial.begin(9600);
-  pinMode(pH_Value, INPUT); 
-} 
- 
-void loop() 
-{ 
-  pH_Value = analogRead(A0); 
-  Voltage = pH_Value * (5.0 / 1023.0); 
-  Serial.println(Voltage); 
-  delay(500); 
-}
