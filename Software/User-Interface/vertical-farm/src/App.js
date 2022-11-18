f56cc3092fe9c1fe5fb83a6dae6c1cd2b31bc86a
Tejpal Lalihiana <tejpalssb23@gmail.com>
2022-11-17 19:27:28 -0800

    Complete header

--- Software/User-Interface/vertical-farm/src/App.js
+++ Software/User-Interface/vertical-farm/src/App.js
@@ -1,20 +1,22 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
+
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
-import Readings from "./pages/Readings"
+import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import Graphs from "./pages/Graphs"
 import ForgotPassword from "./pages/ForgotPassword"
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
       <Router>
+        <Header/> 
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/readings" element={<Readings />} />
+          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/graphs" element={<Graphs />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
--- Software/User-Interface/vertical-farm/src/pages/Readings.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Readings() {
-  return (
-    <div>Readings</div>
-  )
-}
