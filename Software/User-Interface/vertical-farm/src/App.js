8c004054f4721bda9f1e4508c538127c55825fa4
Tejpal Lalihiana <tejpalssb23@gmail.com>
2022-11-17 18:14:29 -0800

    Create pages and routes

--- Software/User-Interface/vertical-farm/src/App.js
+++ Software/User-Interface/vertical-farm/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Readings from "./pages/Readings"
+import Profile from "./pages/Profile"
+import Graphs from "./pages/Graphs"
+import ForgotPassword from "./pages/ForgotPassword"
 function App() {
   return (
     <>
-      <h1 className='text-2xl bg-red-200'>Hello!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/readings" element={<Readings />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/graphs" element={<Graphs />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
