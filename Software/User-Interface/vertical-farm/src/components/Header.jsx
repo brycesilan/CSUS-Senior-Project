d95e3bea3e4a42d2431e8f6b0e98294e9ea7877a
Tejpal Lalihiana <tejpalssb23@gmail.com>
2022-11-19 06:37:56 -0800

    Add Spinner component

--- Software/User-Interface/vertical-farm/src/components/Header.jsx
+++ Software/User-Interface/vertical-farm/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
   }
 
   return (
-    <div className="bg-white border-b shadow-md sticky top-0 z-50">
+    <div className="bg-white border-b shadow-md sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
--- Software/User-Interface/vertical-farm/src/components/PrivateRoute.jsx
+++ Software/User-Interface/vertical-farm/src/components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { Outlet, Navigate } from "react-router-dom"
 import { useAuthStatus } from "../hooks/useAuthStatus"
+import Spinner from "./Spinner";
 
 export default function PrivateRoute() {
     const { loggedIn, checkStatus } = useAuthStatus();
 
     if(checkStatus) {
-        return <h3>Loading...</h3>
+        return <Spinner />
     }
     return (
         loggedIn ? <Outlet /> : <Navigate to="/sign-in/" />
