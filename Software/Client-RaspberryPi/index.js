827787178bddb31a6ad36f6c39a7a3ee852cc07b
Tejpal Lalihiana <tejpalssb23@gmail.com>
2022-11-22 03:37:27 -0800

    Basic write to firestore working

--- Software/Client-RaspberryPi/index.js
+++ Software/Client-RaspberryPi/index.js
@@ -1,7 +1,5 @@
 const { sensorValues } = require('./readings.js');
 const { getTimestamps } = require('./dates.js')
-const { initializeApp } = require('firebase/app')
-const { db } = require('./firebase-config.js');
 const { getFirestore, collection,
     addDoc,
     query,
@@ -10,11 +8,39 @@ const { getFirestore, collection,
     onSnapshot,
     setDoc,
     updateDoc,
-    doc,} = require('firebase/firestore');
+    doc,
+    getDocs,} = require('firebase/firestore');
+const { initializeApp } = require('firebase/app')
 
+const configs = require('./firebase-config.js');
+const firebaseConfig = configs.firebaseConfig;
+const UserUID = configs.UserUID;
+
+console.log(firebaseConfig);
+console.log(UserUID);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+async function UploadToFirebase(data) {
+    const ref = doc(db, 'Users', UserUID, 'UserData', 'Current');
+    const UserCol = collection(ref, 'new');
+    const UserDoc = doc(UserCol, UserUID);
+    // const Users = await getDocs(UserCol);
+    // const UsersList = Users.docs.map(doc => doc.data());
+    const temp = {};
+    Object.assign(temp, data);
+    const tester = await addDoc(UserCol, temp);
+    //console.log(tester);
+}
 
 setInterval(() => {sensorValues().then((obj) => {
     const timestamps = getTimestamps();
+    
+    obj.timestamp = timestamps.timestamp;
     console.log(obj);
     console.log(timestamps);
+    UploadToFirebase(obj);
+
 })}, 5000);
+
