a9495fcec47cc3b13c0d81b52609cc9bd3599cb4
Tejpal Lalihiana <tejpalssb23@gmail.com>
2022-12-13 01:40:10 -0800

    Add comments to Client code

---
+++ Software/User-Interface/vertical-farm/src/firebase-config-sample.js
@@ -0,0 +1,16 @@
+//Replace values for keys accordingly
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID"
+};
+
+const UserUID = "USER_ID";
+
+module.exports = {firebaseConfig, UserUID};
